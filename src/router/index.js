1b890882393392c81407f391552db9ecb7e52dd0
charlesdpj <charlesdpj78@gmail.com>
2023-10-02 00:49:03 +0100

    adding stub components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Body from './../components/Body.vue'
 import Login from './../components/Login.vue'
+import Blog from './../components/Blog.vue'
+import Blogs from './../components/Blogs.vue'
+import BlogsAdmin from './../components/BlogsAdmin.vue'
+import BlogEdit from './../components/BlogEdit.vue'
+import User from './../components/UserEdit.vue'
+import Users from './../components/Users.vue'
 
 const routes = [
     {
@@ -12,7 +18,38 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: Login,
-    }
+    },
+    {
+        path: '/blogs',
+        name: 'Blogs',
+        component: Blogs,
+    },
+    {
+        path: '/blogs/:blogName',
+        name: 'Blog',
+        component: Blog,
+    },
+    {
+        path: '/admin/blogs',
+        name: 'BlogsAdmin',
+        component: BlogsAdmin,
+    },
+    {
+        path: '/admin/blogs/:blogID',
+        name: 'BlogEdit',
+        component: BlogEdit,
+    },
+    {
+        path: '/admin/users',
+        name: 'Users',
+        component: Users,
+    },
+    {
+        path: '/admin/users/:userid',
+        name: 'User',
+        component: User,
+    },
+
 ]
 
 const router = createRouter({history: createWebHistory(), routes})
